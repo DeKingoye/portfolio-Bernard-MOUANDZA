16837ecbb5d7b31734e80f2a94cc0b481b4a39bb
DeKingoye <yannmouandza3@gmail.com>
2022-07-20 11:20:41 +0200

    add

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -45,7 +45,7 @@ function About() {
         <div className="col-sm-5 col-12 container__profile"></div>
         <div className="col-sm-7 col-12 container__quality">
           <p className="text-justify">
-          hJe suis à la recherche d'une alternance
+          Je suis à la recherche d'une alternance
           dès le mois de septembre pour mes deux ans en MASTERE CTO et TECH Lead à HETIC.
           Mon objectif à terme est de devenir 
           un expert en développement, capable de participer à toutes 
